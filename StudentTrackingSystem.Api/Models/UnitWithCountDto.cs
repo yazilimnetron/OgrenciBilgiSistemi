1ac0c3a3d5557639fd9d3ca33c07d3bab82be649
yazilimnetron <yazilimnetron@gmail.com>
2026-02-01 11:09:51 +0300

    Delete StudentTrackingSystem.Api directory
    Delete StudentTrackingSystem.Api directory

--- StudentTrackingSystem.Api/Models/UnitWithCountDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StudentTrackingSystem.Api.Models
-{
-    public class UnitWithCountDto
-    {
-        public Unit UnitData { get; set; }
-        public int StudentCount { get; set; }
-    }
-}
